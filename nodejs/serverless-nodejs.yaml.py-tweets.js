0f6e9b2f3575b1d21f663faa3e17fed60a87419c
Wilhelm Wonigkeit <wwonigkeit@iqconsult.com.au>
2022-11-30 09:46:03 +1100

    JSON stringify on JS

--- nodejs/serverless-nodejs.yaml.py-tweets.js
+++ nodejs/serverless-nodejs.yaml.py-tweets.js
@@ -30,8 +30,8 @@ async function getRequest() {
     })
 
     if (res.body) {
-        return JSON.stringify(res.body);
-        //return res.body
+        // return JSON.stringify(res.body);
+        return res.body
     } else {
         throw new Error('Unsuccessful request');
     }
